91f0d7ab34434340516c3ac30698ad8f454cf8f6
aberimen <berimen63@gmail.com>
2021-05-17 15:12:28 +0300

    initial project

---
+++ src/test/java/com/aberimen/swagger/Swagger3ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aberimen.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swagger3ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
